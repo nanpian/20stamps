17a47a3956c9b4a208ac468f77ae80ee6d1bb330
南京－朱德伟 <zhudewei@jd.com>
2015-08-08 17:34:39 +0800

    add back in payment info review activity

--- stamp20/src/com/stamp20/app/activities/PaymentInfoActivity.java
+++ stamp20/src/com/stamp20/app/activities/PaymentInfoActivity.java
@@ -2,8 +2,10 @@ package com.stamp20.app.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
@@ -11,12 +13,14 @@ import android.widget.LinearLayout;
 import com.stamp20.app.R;
 import com.stamp20.app.fragments.ShippingAddressFragment;
 import com.stamp20.app.util.FontManager;
+import com.stamp20.app.util.Log;
 
-public class PaymentInfoActivity extends Activity implements OnCheckedChangeListener {
+public class PaymentInfoActivity extends Activity implements View.OnClickListener, OnCheckedChangeListener {
 
     private ShippingAddressFragment mShippingAddressFragment;
     private CheckBox sameAsCheckBox;
     private TextView headerTitle;
+    private ImageView headerPrevious;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +36,30 @@ public class PaymentInfoActivity extends Activity implements OnCheckedChangeList
         sameAsCheckBox = (CheckBox) findViewById(R.id.checkbox_same_as_shipping_address);
         sameAsCheckBox.setOnCheckedChangeListener(this);
         if (mShippingAddressFragment == null) {
-            mShippingAddressFragment = (ShippingAddressFragment) getFragmentManager().findFragmentById(R.id.shipping_address_fragment);
+            mShippingAddressFragment = (ShippingAddressFragment) getFragmentManager().findFragmentById(
+                    R.id.shipping_address_fragment);
             mShippingAddressFragment.setVisible(!sameAsCheckBox.isChecked());
         }
+
+        headerPrevious = (ImageView) findViewById(R.id.header_previous);
+        headerPrevious.setOnClickListener(this);
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if ((CheckBox) buttonView == sameAsCheckBox) {
             mShippingAddressFragment.setVisible(!isChecked);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        Log.d(this, "onClick, v" + v + ", " + v.getId());
+        switch (v.getId()) {
+        case R.id.header_previous:
+            finish();
+            break;
         }
     }
 
